709971763283971a5ae5f18fdd7b433f848c2ceb
Krzysztof Kukiz <kukizk@gmail.com>
2017-05-28 20:32:00 +0200

    Stworzenie 3 opszarów roboczych

---
+++ zad/budzet_domowy/index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Krzysztof Kukiz kukizk@gmail.com">
+	<meta name="Keywords" content="">
+	<meta name="Description" content="">
+	<title>Miesięczny Budżet Domowy</title>
+	<link href="" rel="icon" type="image/x-icon">
+	<link rel="stylesheet" href="bin/style/screen.css">
+	
+</head>
+<body>
+<div id="box">
+	<article id="przychody">
+		<header>Przychody</header>
+	</article>
+	<article id="wydatki">
+		<header>Odchody</header>
+	</article>
+	<article id="podsumowanie">
+		<header>Podsumowanie</header>
+	</article>
+</div>
+</body>
+</html>
