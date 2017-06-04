500f37c26989fbeacabf3ff937db8cae35ca8520
Krzysztof Kukiz <kukizk@gmail.com>
2017-06-04 15:31:43 +0200

    od nowa

---
+++ zad/video-tutorial/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="Krzysztof Kukiz kukizk@gmail.com">
+	<!--<meta name="Keywords" content="">-->
+	<!--<meta name="Description" content="">-->
+	<title>Viteo tutorial</title>
+	<!--<link href="" rel="icon" type="image/x-icon">-->
+	<link rel="stylesheet" href="bin/style/screen.css">
+	<script src="../../script/jq.js"></script>
+	<script>
+		
+	</script>
+	
+</head>
+<body>
+<div id="box">
+dd s y w
+</div>
+</body>
+</html>
